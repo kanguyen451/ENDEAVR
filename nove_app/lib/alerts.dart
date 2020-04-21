2e014b4c1a977270ccb2493ad138e9251fa82a8d
Siddarth <siddarth.pandian@gmail.com>
2020-04-21 13:48:22 -0500

    Added alerts page

--- nove_app/lib/alerts.dart
+++ nove_app/lib/alerts.dart
@@ -7,8 +7,41 @@ class Alerts extends StatelessWidget {
           title: Text("Alerts"),
           backgroundColor: Color(0xff2389FF),
         ),
-        body: Center(
-          child: Text("Alerts page under construction"),
-        ));
+        body: ListView(
+          children: ListTile.divideTiles( //          <-- ListTile.divideTiles
+              context: context,
+              tiles: [
+                ListTile(
+                  title: Row(
+                      children: <Widget>[
+                        Icon(Icons.warning),
+                        Text("  Flooding in North st.")
+                      ]
+                  ),
+                  trailing: Icon(Icons.map),
+                ),
+                ListTile(
+                  title: Row(
+                      children: <Widget>[
+                        Icon(Icons.whatshot),
+                        Text("  Fire in Creek ln.")
+                      ]
+                  ),
+                  trailing: Icon(Icons.map),
+                ),
+                ListTile(
+                  title: Row(
+                      children: <Widget>[
+                        Icon(Icons.train),
+                        Text("  Train blocked N main st.")
+                      ]
+                  ),
+                  trailing: Icon(Icons.map),
+                ),
+              ]
+          ).toList(),
+        )
+    );
   }
 }
+
